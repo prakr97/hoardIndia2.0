a2c91e3eca192ec8e366b6d193f0f25ff23ffb39
prakhar.singh <prakhar.singh@extramarks.com>
2024-05-28 00:55:58 +0530

    changing port

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const path = require('path');
 
 // const chartRoutes = require('./routes/chartRoutes.js')
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 6000;
 
 app.use(cors()); 
 app.use(bodyParser.json()); 
