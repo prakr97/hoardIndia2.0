0469d3eb2e54efff138c964c838a259ef69c1482
prakhar.singh <prakhar.singh@extramarks.com>
2024-05-28 13:09:45 +0530

    url changes

--- frontend/src/services/otpServices.js
+++ frontend/src/services/otpServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const url = 'http://localhost:5000'
-const url = 'http://5.161.224.84:5000/'
+const url = 'http://5.161.224.84:5000'
 
 export const sendOtp = async(params) => {
     return await axios.post(`${url}/otp/sendOtp`,{params})
